ee7add4d9bbca1448fb426fd282908fba801c74c
Ira <irarad1994@gmail.com>
2020-09-09 23:49:03 +0300

    Updates

---
+++ precache-manifest.45ab222dfc31ec1ecf8e8287bfe07269.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5711103be7e13af0a5839cdbf81e25",
+    "url": "/HW16-advanced/index.html"
+  },
+  {
+    "revision": "e760f576cd2f7b00e045",
+    "url": "/HW16-advanced/static/css/main.244107c2.chunk.css"
+  },
+  {
+    "revision": "7ca4cf787173e7c6395c",
+    "url": "/HW16-advanced/static/js/2.a8727e54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW16-advanced/static/js/2.a8727e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e760f576cd2f7b00e045",
+    "url": "/HW16-advanced/static/js/main.5381fe03.chunk.js"
+  },
+  {
+    "revision": "3c33d0482e37e7a286bc",
+    "url": "/HW16-advanced/static/js/runtime-main.adeb33a6.js"
+  },
+  {
+    "revision": "45e7f50183425de5f7dcdad02a4f4df5",
+    "url": "/HW16-advanced/static/media/girl.45e7f501.svg"
+  },
+  {
+    "revision": "a01f134c77daa3dc2142062572dd69f2",
+    "url": "/HW16-advanced/static/media/man.a01f134c.svg"
+  },
+  {
+    "revision": "0a607ec2b1805d6ab361e309ca53a5c9",
+    "url": "/HW16-advanced/static/media/question.0a607ec2.svg"
+  }
+]);
